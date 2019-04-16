282277f503feefa01dbeb1c694a6608a27078f28
Tyler Landry <tyler.landry1996@outlook.com>
2019-04-16 01:32:14 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -17,9 +17,10 @@ end
 
 def while_iterator(number_of_times)
   counter = 0 
-  while counter > 
-  phrase = "Welcome to Flatiron School's Web Development Course!"
-  
+  while counter > number_of_times
+  puts "Welcome to Flatiron School's Web Development Course!" 
+  counter += 1
+  end
 end
 
 #def until_iterator(number_of_times)
