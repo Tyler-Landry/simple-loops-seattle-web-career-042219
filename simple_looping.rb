bdf01b48e9f8ab9936b81811cc7f63f55e0a5a62
Tyler Landry <tyler.landry1996@outlook.com>
2019-04-16 01:06:30 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -2,10 +2,10 @@ def loop_iterator(number_of_times)
   counter = 0
   loop do 
     counter += 1
+    puts "Welcome to Flatiron School's Web Development Course!"
     if counter == number_of_times
       break
     end
-  puts "Welcome to Flatiron School's Web Development Course!"
 end
 
 def times_iterator(number_of_times)
